a3a2100946992374aded3dd1b18d2b59e4398fab
abmoore92 <abmoore92@gmail.com>
2017-06-02 16:10:50 +0100

    neatened up functions
    added a cleaning function to remove common tables and vacuum the database. results in ~30% file size decrease

--- data_management.py
+++ data_management.py
@@ -4,6 +4,39 @@ import os
 from reporting import *
 import shutil
 
+def getMemoryUsage(tableList,con):
+    dataUsage = {}
+    for t in tableList:
+        dataUsage[t] = get(t,con).memory_usage().sum()
+#getMemoryUsage(tableUnique['common'],connections[0])
+
+def makeDirectory(mainPath,mergePath):
+    if not os.path.exists(mainPath+mergePath):
+        os.makedirs(mainPath+mergePath)
+
+def replaceString(myString, dict):
+    for k in dict.keys():
+        myString = myString.replace(k, dict[k])
+    return myString
+
+#make a scenarios settings table in a database with columns scenario/identifier/value
+def makeScenariosTable(connection):
+    cursor = connection.cursor()
+    cursor.execute("""CREATE TABLE 
+                        IF NOT EXISTS scenarios (
+                        scenario string NOT NULL,
+                        identifier string NOT NULL,
+                        value string NOT NULL
+                        );""")
+
+#scenarios table in sqlite database
+def writeToScenarioTable(connection,scenarioDatabasefile):
+    scenarioSettings = dbFileSettings(scenarioDatabasefile)
+    for id in scenarioSettings.keys():
+        cursor.execute("INSERT INTO scenarios (scenario,identifier,value) VALUES ('%s','%s','%s');"%(scen,id,scenarioSettings[id]))
+
+
+
 mainPath   = r'C:\Users\ucqba01\Documents\Local Data\testing databases/'
 mergePath = r'\merged'
 databases = [f for f in os.listdir(mainPath) if f[-3:] == '.db']
@@ -13,8 +46,8 @@ print(databases)
 tables = getList(connections[0])
 print(tables)
 
+#this loop is very slow
 tableUnique = {'common':[],'specific':[]}
-
 for t in tables:
     tableData0 = get(t,connections[0])
     tableData1 = get(t,connections[1])
@@ -26,57 +59,36 @@ for t in tables:
 print('common tables',len(tableUnique['common']))
 print('specific tables',len(tableUnique['specific']))
 
-def getMemoryUsage(tableList,con)
-    dataUsage = {}
-    for t in tableList:
-        dataUsage[t] = get(t,con).memory_usage().sum()
-
-def makeDirectory(mainPath,mergePath):
-    if not os.path.exists(mainPath+mergePath):
-        os.makedirs(mainPath+mergePath)
-
-def replaceString(myString, dict):
-    for k in dict.keys():
-        myString = myString.replace(k, dict[k])
-    return myString
-
-
-#getMemoryUsage(tableUnique['common'],connections[0])
-mergeCon = makeDataBase(mainPath+mergePath,'merged.db')
-
 makeDirectory(mainPath,mergePath)
-
-mergeDataBaseFile = '/merge3.db'
+mergeDataBaseFile = '/merge5.db'
 
 shutil.copy(mainPath+'/'+databases[0],mainPath+mergePath+mergeDataBaseFile)
+mergeCon = sq.connect(mainPath+mergePath+mergeDataBaseFile)
 
-scenarios = {}
-scenarioSettings = pd.DataFrame(columns=['scenario','identifier','value'])
+makeScenariosTable(mergeCon)
 
+#make a dict of scenario names for each database in the list
+scenarioNames = {}
 for db in databases:
-    scenarios[db] = replaceString(db,{'hR_m_2002_waves':'w','_newfuelcost.db':''})
-
-#this function is very user specific
-identifiersDict = {'waves'}
-
-con = sq.connect(mainPath+mergePath+mergeDataBaseFile)
-
+    scenarioNames[db] = replaceString(db,{'hR_m_2002_waves':'w','_newfuelcost.db':''})
 
 for t in tableUnique['specific']:
-    scen=scenarios[databases[0]]
-    colCursor = con.execute('select * from %s'%t)
-    cursor = con.cursor()
+    scen=scenarioNames[databases[0]]
+    colCursor = mergeCon.execute('select * from %s'%t)
+    cursor = mergeCon.cursor()
     if 'scenario' not in [col[0] for col in colCursor.description]:
         cursor.execute("ALTER TABLE %s ADD COLUMN scenario string;"%t)
+        print('adding scenario column to %s'%t)
     cursor.execute("UPDATE %s SET scenario='%s'" %(t,scen))
-    cursor.execute("""CREATE TABLE 
-                        IF NOT EXISTS scenarios (
-                        scenario string NOT NULL,
-                        identifier string NOT NULL,
-                        value string NOT NULL
-                        );""")
-    scenarioSettings = dbFileSettings(databases[0])
-for id in scenarioSettings.keys():
-    cursor.execute("INSERT INTO scenarios (scenario,identifier,value) VALUES ('%s','%s','%s');"%(scen,id,scenarioSettings[id]))
 
+writeToScenarioTable(mergeCon,databases[0])
 
+for db in databases:
+    if db != databases[0]:
+        cursor.execute("ATTACH DATABASE \"%s\" AS %s;"%(mainPath+'\\'+db,'toAppend'))
+
+def removeCommonTables(connection)
+    for t in tableUnique['common']:
+        cursor = connection.cursor()
+        cursor.execute("DROP TABLE %s;" %t)
+    cursor.execute("VACUUM")
